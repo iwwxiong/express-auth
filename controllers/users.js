685f1bb751935d84889ca1e30f3eaf41407d11d5
wwx <xhq@yiban.cn>
2016-02-19 17:15:00 +0800

    基本逻辑完成

--- controllers/users.js
+++ controllers/users.js
@@ -2,6 +2,9 @@
 /*
  * Created by wwxiong on 2016/01/25.
  */
+var model = require('./models');
+var Group = model.Group;
+var Permission = model.Permission;
 var proxy = require('./proxy');
 var userProxy = proxy.User;
 
@@ -14,6 +17,7 @@ var userProxy = proxy.User;
     'data': '' # data items
   }
 */
+
 /*
  * 查看所有用户
  */
@@ -32,13 +36,13 @@ exports.getUsers = function(res, req, next){
  * 新增用户信息
  */
 exports.postUser = function(res, req, next){
-  let username = req.params.username;
-  let name = req.params.name;
-  let age = req.params.age;
-  let sex = req.params.sex;
-  let age = req.params.age;
-  let email = req.params.email;
-  let telephone = req.params.telephone;
+  let username = res.body.username;
+  let name = res.body.name;
+  let age = res.body.age;
+  let sex = res.body.sex;
+  let age = res.body.age;
+  let email = res.body.email;
+  let telephone = res.body.telephone;
   userProxy.getUserByUsername(username).then(function(user){
     if(user)
       return res.send({'result': false, 'message': '用户名已存在'});
@@ -56,12 +60,157 @@ exports.postUser = function(res, req, next){
  * 根据用户ID获取用户
  */
 exports.getUser = function(res, req, next){
-  var userId = req.params.userId;
+  var userId = res.params.userId;
   user.Proxy.getUserById(userId).then(function(user){
     return res.send({
       'result': true,
       'data': user
     });
+  }).catch(function(err){
+    return next(err);
+  });
+};
+
+/*
+ * 获取用户群组信息
+ */
+exports.getGroups = function(res, req, next){
+  var userId = req.params.userId;
+  Group.find({}, function(err, groups){
+    if(groups){
+      return res.send({
+        'result': true,
+        'data': data
+      });
+    }
+  });
+};
+
+/*
+ * 添加用户群组
+ */
+exports.postGroup = function(res, req, next){
+  var name = res.body.name;  //课群名称
+  Group.findOne({'name': name}, function(err, group){
+    if(err, group){
+      return res.send({'result': false, 'message': '课群名称已存在'});
+    }
+    newGroup(name).then(function(data){
+      return res.send({'result': true, 'data': group});
+    }).catch(function(err){
+      return next(err);
+    });
+  });
+};
+
+/*
+ * 根据群组ID获取群组信息
+ */
+exports.getGroupById = function(res, req, next){
+  var groupId = res.params.groupId;
+  Group.findOne({_id: groupId}, function(err, group){
+    if(group){
+      return res.send({'result': true, 'data': group});
+    }
+  });
+};
+
+/*
+ * 根据群组ID修改群组信息
+ */
+exports.putGroup = function(res, req, next){
+  var groupId = res.params.groupId;
+  var name = res.body.name;
+  Group.findOne({_id: groupId}, function(err, group){
+    if(group){
+      group.name = name;
+      group.save(function(err){
+        if(!err){
+          return res.send({'result': true, 'data': group});
+        }
+      });
+    }
+  });
+};
+
+/*
+ * 获取权限列表信息
+ */
+exports.getPermissions = function(res, req, next){
+  Permission.find({}, function(err, permissions){
+    if(permissions){
+      return res.send({'result': true, 'data': permissions});
+    }
+  });
+};
+
+/*
+ * 获取权限列表信息
+ */
+exports.postPermission = function(res, req, next){
+  var name = res.body.name;
+  var description = res.body.description;
+  Permission.findOne({'name': name}, function(err, permission){
+    if(permission){
+      return res.send({'result': false, 'message': '权限已存在'});
+    }
+    newPermission(name, description).then(function(p){
+      return res.send({'result': true, 'data': p});
+    });
+  });
+};
+
+/*
+ * 获取权限列表信息
+ */
+exports.getPermissionById = function(res, req, next){
+  var permissionId = req.params.permissionId;
+  Permission.findOne({_id: permissionId}, function(permission){
+    if(!permission){
+      return res.send({'result': false, 'data': '权限未找到'});
+    }
+    return res.send({'result': true, 'data': permission});
+  });
+};
+
+/*
+ * 修改权限
+ */
+exports.putPermission = function(res, req, next){
+  var permissionId = req.params.permissionId;
+};
+
+/*
+ * 获取用户所有权限
+ */
+exports.getUserPermissions = function(res, req, next){
+  var userId = res.params.userId;
+  userProxy.getUserPermissions(userId).then(function(data){
+    res.send({'result': true, 'data': data});
+  }).catch(function(err){
+    return next(err);
+  });
+};
+
+/*
+ * 获取用户所属组
+ */
+exports.getUserGroups = function(res, req, next){
+  var userId = res.params.userId;
+  userProxy.getUserGroups(userId).then(function(data){
+    res.send({'result': true, 'data': data});
+  }).catch(function(err){
+    return next(err);
+  });
+};
+
+/*
+ * 获取组权限
+ */
+exports.getGroupPermissions = function(res, req, next){
+  var groupId = res.params.groupId;
+  userProxy.getGroupPermissions(groupId).then(function(data){
+    res.send({'result': true, 'data': data});
   }).catch(function(err){
     return next(err);
   });
--- routes/users.js
+++ routes/users.js
@@ -10,26 +10,23 @@ var User = require('./controllers');
 router.get('/users', User.getUsers);
 router.post('/users', User.postUser);
 router.get('/user/:userId', User.getUser);
-router.delete('/user/:userId', User.deleteUser);
+//router.delete('/user/:userId', User.deleteUser);
 /* 群组路由 */
 router.get('/groups', User.getGroups);
 router.post('/groups', User.postGroup);
 router.get('/group/:groupId', User.getGroupById);
 router.put('/group/:groupId', User.putGroup);
-router.delete('/group/:groupId', User.deleteGroup);
+//router.delete('/group/:groupId', User.deleteGroup);
 /* 权限路由 */
 router.get('/permissions', User.getPermissions);
 router.post('/permissions', User.postPermission);
 router.get('/permission/:permissionId', User.getPermissionById);
 router.put('/permission/:permissionId', User.putPermission);
-router.delete('/permission/:permissionId', User.deletePermission);
+//router.delete('/permission/:permissionId', User.deletePermission);
 /* 用户权限管理路由 */
 router.get('/user/:userId/permissions', User.getUserPermissions);
-router.get('/user/:userId/permission/:permissionId', User.getUserPermission);
-router.delete('/user/:userId/permission/:permissionId', User.deleteUserPermission);
+//router.delete('/user/:userId/permission/:permissionId', User.deleteUserPermission);
 router.get('/user/:userId/groups', User.getUserGroups);
-router.get('/user/:userId/group/:groupId', User.getUserGroup);
 router.get('/group/:groupId/permissions', User.getGroupPermissions);
-router.get('/group/:groupId/permission/:permissionId', User.getGroupPermission);
 
 module.exports = router;
